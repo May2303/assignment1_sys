254493ffc02111eb686a82ee01df8d2c2929a3bf
Shira Yogev <54641048+May2303@users.noreply.github.com>
2024-01-28 18:01:54 +0200

    NumClass.h

---
+++ NumClass.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int isPrime(int num);
+int isPalindrome(int num);
+int isPalindromeRec(int num);
+int isArmstrong(int num);
+int isArmstrongRec(int num);
+int isStrong(int num);
